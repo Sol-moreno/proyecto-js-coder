f981487544b705fc357c3660d30f878dbdef8ab1
maria soledad moreno <morenosoledad24@gmail.com>
2023-04-23 22:39:24 -0300

    pre entrega 3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const container = document.querySelector(".container")
-
+const card = document.querySelector(".card")
+const imgCarrito = document.querySelector(".carrito")
+const inputSerch= document.querySelector ("#inputSearch")
 const img = document.getElementById("logo")
 const rutaImagenLogo = "img/descarga (1).jpg"
 
@@ -13,48 +14,63 @@ img.title = "Logo de Estética y Salud"
 
 
 
+
+
+
 // cards generadas automaticamente
 
      function retornarCardsHTML(producto) {
     //    template literal 
 
-    return ` <div class="container">
-    <img src="img/TIPO_IMPLANTES.jpg"${producto.img} class="foto" alt="...">
-      <div class="card-body">
-       <h5 class="card-title">${producto.categoria}</h5>
-       <p class="card-text">${producto.descripcion}</p>
-        <p class="dolares">usd$${producto.importe} </p>
-      <button class="enviar">Agregar al carrito</button>
-      </div>
-     </div>
-     `
+    return `   <div class="card" style="width: 18rem;">
+    <img src="img/protesis-lisas-o-texturizadas.jpg.webp" ${producto.img}class="foto" alt="...">
+    <div class="card-body">
+      <h5 class="card-title"> ${producto.categoria} </h5>
+      <p class="card-text"> ${producto.descripcion} </p>
+       <p class="dolares"> usd$ ${producto.importe}</p>
+      <button type="submit" id="boton"> Enviar al carrito</button>
+    </div>
+    </div> `
+
 
  }
 
 
- const arrayProductos = [{img : "img/TIPO_IMPLANTES.jpg", id: 1, categoria: 'protesis mamarias', descripcion:'protesis anatomicas texturizadas perfil xp',importe: 1250 , stock:  20, cantidad: 1 },
- {img :"img/TIPO_IMPLANTES.jpg" , id:  2, categoria: ' Acido hialuronico', descripcion: ' relleno para arrugas faciales y surcos ', importe: 150 ,  stock: 10  ,cantidad:1},
- {img : "img/TIPO_IMPLANTES.jpg" , id:  3, categoria: 'toxina butulinica', descripcion: 'para relajar arrugas faciales , para hiperhidrosis',          importe: 200 ,  stock:  6 ,cantidad:1 },
- {img : "img/TIPO_IMPLANTES.jpg" , id:  4, categoria: 'filorga',           descripcion: 'inyeccion de vitaminas , terapia ortomolescular',             importe: 300,  stock:  15 , cantidad:1  },
- {img : "img/TIPO_IMPLANTES.jpg" , id:  5, categoria: 'colageno',          descripcion: 'presentacion en polvo, mejora el aspecto general de la piel , uñas y pelo',  importe: 100 , stock: 30, cantidad:1},
- {img : "img/TIPO_IMPLANTES.jpg" , id:  6, categoria: 'acido hialuronico', descripcion: 'acido de alta densidad para modelar nariz',                   importe: 230,  stock: 5 ,cantidad:1  },
- {img : "img/TIPO_IMPLANTES.jpg" , id:  7, categoria: 'colageno',          descripcion: 'presentacion liquida en gotero',                              importe: 130 ,  stock: 30,cantidad:1 },
- {img : "img/TIPO_IMPLANTES.jpg" , id:  8, categoria: 'protesis mamarias', descripcion: 'protesis redondas texturizadas tamaños desde 200 a 300 xp' ,  importe: 1450 , stock: 14 ,cantidad:1},
- {img : "img/TIPO_IMPLANTES.jpg" , id:  9, categoria: 'acido hialuronico', descripcion: 'densidad alta relleno para gluteos' ,                         importe:  600 ,  stock: 4,cantidad:1 },
- {img : "img/TIPO_IMPLANTES.jpg" , id: 10, categoria: 'protesis mamarias', descripcion: 'protesis lisas redondas tamaños desde 350 a 500 xp' ,         importe: 1400 , stock: 15, cantidad:1 }
+ const arrayProductos = [{img : "img/TIPO_IMPLANTES.jpg", id: 1, categoria: 'protesis mamarias', descripcion:'Protesis anatomicas texturizadas perfil xp', importe: 1250 , stock:  20, cantidad: 1 },
+ {img :"img/TIPO_IMPLANTES.jpg" , id:  2, categoria: ' Acido hialuronico', descripcion: ' Relleno para arrugas faciales y surcos ', importe: 150 ,  stock: 10  ,cantidad:1},
+ {img : "img/TIPO_IMPLANTES.jpg" , id:  3, categoria: 'toxina butulinica', descripcion: 'Para relajar arrugas faciales , para hiperhidrosis',          importe: 200 ,  stock:  6 ,cantidad:1 },
+ {img : "img/TIPO_IMPLANTES.jpg" , id:  4, categoria: 'filorga',           descripcion: 'Inyeccion de vitaminas , terapia ortomolescular',             importe: 300,  stock:  15 , cantidad:1  },
+ {img : "img/TIPO_IMPLANTES.jpg" , id:  5, categoria: 'colageno',          descripcion: 'Presentacion en polvo, mejora el aspecto general de la piel , uñas y pelo',  importe: 100 , stock: 30, cantidad:1},
+ {img : "img/TIPO_IMPLANTES.jpg" , id:  6, categoria: 'acido hialuronico', descripcion: 'Acido de alta densidad para modelar nariz',                   importe: 230,  stock: 5 ,cantidad:1  },
+ {img : "img/TIPO_IMPLANTES.jpg" , id:  7, categoria: 'colageno',          descripcion: 'Presentacion liquida en gotero',                              importe: 130 ,  stock: 30,cantidad:1 },
+ {img : "img/TIPO_IMPLANTES.jpg" , id:  8, categoria: 'protesis mamarias', descripcion: 'Protesis redondas texturizadas tamaños desde 200 a 300 xp' ,  importe: 1450 , stock: 14 ,cantidad:1},
+ {img : "img/TIPO_IMPLANTES.jpg" , id:  9, categoria: 'acido hialuronico', descripcion: 'Densidad alta relleno para gluteos' ,                         importe:  600 ,  stock: 4,cantidad:1 },
+ {img : "img/TIPO_IMPLANTES.jpg" , id: 10, categoria: 'protesis mamarias', descripcion: 'Protesis lisas redondas tamaños desde 350 a 500 xp' ,         importe: 1400 , stock: 15, cantidad:1 }
  
                    ]
+
 function cargarProductos (array) {
           
-          if(container){
-          container.innerHTML= "" ;
+          if(card){
+          card.innerHTML= "" ;
           array.forEach((producto) => {
-          container.innerHTML += retornarCardsHTML (producto);
+          card.innerHTML += retornarCardsHTML (producto);
           } ) }
         }
         
 
 cargarProductos(arrayProductos)
+
+
+function filtrarProductos(valor){
+    let resultado = arrayProductos.filter(producto => producto.categoria.toLowerCase().includes(valor.tolowerCase()))
+   if(resultado.length > 0) {cargarProductos (resultado)}
+}
+
+inputSerch.addEventListener("search", (e)=> {
+    filtrarProductos(e.target.value)
+})
+
 
 
 
@@ -116,4 +132,6 @@ recuperarDatosDeFormulario()
 
 
 botonEnviar.addEventListener("click", guardarFormularioEnLS)
+
+
 
